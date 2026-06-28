bb75a19e369d389e6a40d6ce76a220db2d51701a
slawomir meller <slawomir.meller@gmail.com>
2026-06-28 09:46:50 +0200

    Fix coverage reporting in CI and update import paths in filters module

--- src/us_accidents_etl/transform/filters.py
+++ src/us_accidents_etl/transform/filters.py
@@ -1,16 +1,16 @@
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 
-from src.us_accidents_etl.config.settings import ETLConfig
+from us_accidents_etl.config.settings import ETLConfig
 
 
 def filter_high_severity(df: DataFrame, cfg: ETLConfig) -> DataFrame:
     return df.filter(F.col("Severity") >= cfg.min_severity)
 
-"""
-@TODO: Add more filters here as needed, e.g., filter by state, date range, etc.
-"""
+
+def filter_weather_conditions(df: DataFrame, cfg: ETLConfig) -> DataFrame:
+    return df.filter(F.col("Weather_Condition").isin(cfg.weather_conditions))
 
 
 def apply_etl_filters(df: DataFrame, cfg: ETLConfig) -> DataFrame:
-    return filter_high_severity(df, cfg)
+    return filter_weather_conditions(filter_high_severity(df, cfg), cfg)
